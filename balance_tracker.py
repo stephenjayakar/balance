6b446ce031c9c033e55d6bbcaed18f044750fc10
Stephen Jayakar <sjayakar@plaid.com>
2020-12-18 14:15:07 -0800

    switched to assets

--- balance_tracker.py
+++ balance_tracker.py
@@ -11,11 +11,13 @@ def get_x_days_ago(x):
     other_day = today - datetime.timedelta(days=x)
     return day_format(other_day)
 
+
 class BalanceTracker:
     def __init__(self, client_id, secret, access_token):
         self.plaid_client = plaid.Client(client_id, secret, 'development')
         self.access_token = access_token
         self._response = self.plaid_client.Accounts.balance.get(access_token)
+        self._asset_report_response = None
 
     def print_account_information(self):
         for account in self._response['accounts']:
@@ -42,41 +44,40 @@ class BalanceTracker:
                 return account['balances']['available']
 
     def balances_over_days(self, num_days, _account_id=None):
-        # get complete transactions object
-        today = get_today()
-        start_day = get_x_days_ago(num_days)
-        response = self.plaid_client.Transactions.get(self.access_token, start_day, today, count=500)
-
-        transactions = response['transactions']
-        num_transactions = response['total_transactions']
-        print(f'Number of transactions: {num_transactions}')
-        num_received_transactions = len(transactions)
-        while num_received_transactions < num_transactions:
-            offset = num_received_transactions
-            next_response = None
-            while not next_response:
-                try:
-                    next_response = self.plaid_client.Transactions.get(self.access_token, start_day, today, count=500, offset=offset)
-                    num_received_transactions += len(next_response['transactions'])
-                    transactions.extend(next_response['transactions'])
-                except:
-                    time.sleep(1)
-
-        # if no account id is provided, use the one with the max balance by default
         account_id = _account_id if _account_id else self._max_account_id()
-        # compute balances over days
-        balance = self.current_balance(account_id)
-        balances = [balance]
-        for i in range(num_days):
-            day = get_x_days_ago(i)
-            balance += self._one_day_of_expenses(day, transactions, account_id)
-            balances.append(balance)
-        return list(reversed(balances))
+        # caching solution as an asset_report includes all account_ids for an item
+        if not self._asset_report_response:
+            pl = self.plaid_client
 
-    def _one_day_of_expenses(self, day, transactions, account_id):
-        total_expenses = 0
-        for txn in transactions:
-            if txn['account_id'] != account_id or txn['date'] != day:
-                continue
-            total_expenses += txn['amount']
-        return total_expenses
+            # generate assets report
+            asset_report_create_response = pl.AssetReport.create(
+                [self.access_token],
+                num_days,
+            )
+            asset_report_token = asset_report_create_response['asset_report_token']
+            # get / wait for asset report
+            response = None
+            while True:
+                try:
+                    response = pl.AssetReport.get(asset_report_token)
+                    break
+                except:
+                    print("sleeping as asset report isn't ready")
+                    time.sleep(0.5)
+            self._asset_report_response = response
+
+        response = self._asset_report_response
+        all_account_info = response['report']['items'][0]['accounts']
+        # select account info for the correct account
+        account = None
+        for acct in all_account_info:
+            if acct['account_id'] == account_id:
+                account = acct
+                break
+        if account == None:
+            raise Exception('something went wrong')
+        historical_balances = account['historical_balances']
+        ret_balances = []
+        for balance in historical_balances:
+            ret_balances.append(balance['current'])
+        return list(reversed(ret_balances))
