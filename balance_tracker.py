58f62e5b54e53efa888731268e2cf6133a73d1ff
Stephen Jayakar <aj12ay@gmail.com>
2020-12-20 15:03:50 -0800

    added schwab tracker plus black formatted my code

--- balance_tracker.py
+++ balance_tracker.py
@@ -2,10 +2,15 @@ import datetime
 import plaid
 import time
 
+
 def day_format(dt):
     return str(dt).split()[0]
+
+
 def get_today():
     return day_format(datetime.datetime.today())
+
+
 def get_x_days_ago(x):
     today = datetime.datetime.today()
     other_day = today - datetime.timedelta(days=x)
@@ -14,34 +19,36 @@ def get_x_days_ago(x):
 
 class BalanceTracker:
     def __init__(self, client_id, secret, access_token):
-        self.plaid_client = plaid.Client(client_id, secret, 'development')
+        self.plaid_client = plaid.Client(client_id, secret, "development")
         self.access_token = access_token
         self._response = self.plaid_client.Accounts.balance.get(access_token)
         self._asset_report_response = None
 
     def print_account_information(self):
-        for account in self._response['accounts']:
-            print(account['account_id'], account['name'], account['balances']['available'])
+        for account in self._response["accounts"]:
+            print(
+                account["account_id"], account["name"], account["balances"]["available"]
+            )
 
     def account_id_from_offset(self, i: int):
-        account = self._response['accounts'][i]
-        return account['account_id']
+        account = self._response["accounts"][i]
+        return account["account_id"]
 
     def _max_account_id(self):
         account_id = None
         balance = 0
-        for account in self._response['accounts']:
-            bal = account['balances']['available']
+        for account in self._response["accounts"]:
+            bal = account["balances"]["available"]
             if bal > balance:
                 balance = bal
-                account_id = account['account_id']
+                account_id = account["account_id"]
         return account_id
 
     def current_balance(self, account_id):
         balance = 0
-        for account in self._response['accounts']:
-            if account['account_id'] == account_id:
-                return account['balances']['available']
+        for account in self._response["accounts"]:
+            if account["account_id"] == account_id:
+                return account["balances"]["available"]
 
     def balances_over_days(self, num_days, _account_id=None):
         account_id = _account_id if _account_id else self._max_account_id()
@@ -54,7 +61,7 @@ class BalanceTracker:
                 [self.access_token],
                 num_days,
             )
-            asset_report_token = asset_report_create_response['asset_report_token']
+            asset_report_token = asset_report_create_response["asset_report_token"]
             # get / wait for asset report
             response = None
             while True:
@@ -67,17 +74,28 @@ class BalanceTracker:
             self._asset_report_response = response
 
         response = self._asset_report_response
-        all_account_info = response['report']['items'][0]['accounts']
+        all_account_info = response["report"]["items"][0]["accounts"]
         # select account info for the correct account
         account = None
         for acct in all_account_info:
-            if acct['account_id'] == account_id:
+            if acct["account_id"] == account_id:
                 account = acct
                 break
         if account == None:
-            raise Exception('something went wrong')
-        historical_balances = account['historical_balances']
+            raise Exception("something went wrong")
+        historical_balances = account["historical_balances"]
         ret_balances = []
         for balance in historical_balances:
-            ret_balances.append(balance['current'])
+            ret_balances.append(balance["current"])
         return list(reversed(ret_balances))
+
+
+class SchwabTracker:
+    def __init__(self, plaid_client, schwab_access_token):
+        self.plaid_client = plaid_client
+        self.schwab_access_token = schwab_access_token
+
+    def current_balance(self) -> float:
+        pl = self.plaid_client
+        response = pl.Accounts.balance.get(self.schwab_access_token)
+        return response["accounts"][0]["balances"]["current"]
